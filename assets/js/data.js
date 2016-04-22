eba3cd930f659cb744805a594a10d44a74dd530c
HexR Web <hello@hexr.org>
2016-04-22 13:10:50 -0500

    Fix URL typo in data template

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,42 +2,42 @@ stashboard.sections =
 [
 	{
 		title :"{{First Section Title}}",
-		url:"/badUrl.html#url={{First Section Webpage}}",
-		extensions:[{title:"{{First Section Sublink Title}}",url:"/badurl.html#url={{First Section Sublink Webpage}}"}]
+		url:"badUrl.html#url={{First Section Webpage}}",
+		extensions:[{title:"{{First Section Sublink Title}}",url:"badurl.html#url={{First Section Sublink Webpage}}"}]
 	},
 	{
 		title:"{{Second Section Title}}",
-		url:"/badUrl.html#url={{Second Section Webpage}}",
-		extensions:[{title:"{{Second Section Sublink Title}}",url:"/badurl.html#url={{Second Section Sublink Webpage}}"}]
+		url:"badUrl.html#url={{Second Section Webpage}}",
+		extensions:[{title:"{{Second Section Sublink Title}}",url:"badurl.html#url={{Second Section Sublink Webpage}}"}]
 	},
 	{
 		title:"{{Third Section Title}}",
-		url:"/badUrl.html#url={{Third Section Webpage}}",
-		extensions:[{title:"{{Third Section Sublink Title}}",url:"/badurl.html#url={{Third Section Sublink Webpage}}"}]
+		url:"badUrl.html#url={{Third Section Webpage}}",
+		extensions:[{title:"{{Third Section Sublink Title}}",url:"badurl.html#url={{Third Section Sublink Webpage}}"}]
 	},
 	{
 		title:"{{Fourth Section Title}}",
-		url:"/badUrl.html#url={{Fourth Section Webpage}}",
-		extensions:[{title:"{{Fourth Section Sublink Title}}",url:"/badurl.html#url={{Fourth Section Sublink Webpage}}"}]
+		url:"badUrl.html#url={{Fourth Section Webpage}}",
+		extensions:[{title:"{{Fourth Section Sublink Title}}",url:"badurl.html#url={{Fourth Section Sublink Webpage}}"}]
 	},
 	{
 		title:"{{Fifth Section Title}}",
-		url:"/badUrl.html#url={{Fifth Section Webpage}}",
-		extensions:[{title:"{{Fifth Section Sublink Title}}",url:"/badurl.html#url={{Fifth Section Sublink Webpage}}"}]
+		url:"badUrl.html#url={{Fifth Section Webpage}}",
+		extensions:[{title:"{{Fifth Section Sublink Title}}",url:"badurl.html#url={{Fifth Section Sublink Webpage}}"}]
 	},
 	{
 		title:"{{Sixth Section Title}}",
-		url:"/badUrl.html#url={{Sixth Section Webpage}}",
-		extensions:[{title:"{{Sixth Section Sublink Title}}",url:"/badurl.html#url={{Sixth Section Sublink Webpage}}"}]
+		url:"badUrl.html#url={{Sixth Section Webpage}}",
+		extensions:[{title:"{{Sixth Section Sublink Title}}",url:"badurl.html#url={{Sixth Section Sublink Webpage}}"}]
 	},
 	{
 		title:"{{Seventh Section Title}}",
-		url:"/badUrl.html#url={{Seventh Section Webpage}}",
-		extensions:[{title:"{{Seventh Section Sublink Title}}",url:"/badurl.html#url={{Seventh Section Sublink Webpage}}"}]
+		url:"badUrl.html#url={{Seventh Section Webpage}}",
+		extensions:[{title:"{{Seventh Section Sublink Title}}",url:"badurl.html#url={{Seventh Section Sublink Webpage}}"}]
 	},
 	{
 		title:"{{Eigth Section Title}}",
-		url:"/badUrl.html#url={{Eigth Section Webpage}}",
-		extensions:[{title:"{{Eigth Section Sublink Title}}",url:"/badurl.html#url={{Eigth Section Sublink Webpage}}"}]
+		url:"badUrl.html#url={{Eigth Section Webpage}}",
+		extensions:[{title:"{{Eigth Section Sublink Title}}",url:"badurl.html#url={{Eigth Section Sublink Webpage}}"}]
 	}
 ];
